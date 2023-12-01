a90223a9d8ea92ce295b62263308c6e6fbebcfee
RiccardoMont <riccardomontalto94@gmail.com>
2023-12-01 19:10:06 +0100

    esercizio pariDispari completato

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-
+/* Esercizio parola palindroma */
 const word = prompt('Inserisci una parola');
 
 function palindrome(word){
@@ -22,3 +22,68 @@ function palindrome(word){
 }
 
 palindrome(word);
+
+
+/*Esercizio pari e dispari*/
+
+
+const gioca = document.getElementById('play');
+const pcScore = document.getElementById('pcScore');
+const totalScore = document.getElementById('totalScore');
+const verdetto = document.getElementById('verdetto');
+
+
+
+function pcHand(){
+
+    const randomNumber = Math.floor(Math.random() * 5) + 1;
+    pcScore.innerHTML = `${randomNumber}`;
+    return randomNumber;
+
+}
+
+
+function somma(a, b){
+    
+    const evenOdd = document.getElementById('evenOdd').value;
+    const result = a + b;
+
+    totalScore.innerHTML = `${result}`;
+
+    if(result % 2 == 0){
+        
+        if (evenOdd == 'pari'){
+
+            verdetto.innerHTML = `<h1 class = 'text-success'>Il totale è pari!</h1>`
+
+        } else {
+
+            verdetto.innerHTML = `<h1 class = 'text-danger'>Il totale è pari!</h1>`
+
+        }
+
+    } else {
+
+        if (evenOdd == 'pari'){
+
+            verdetto.innerHTML = `<h1 class = 'text-danger'>Il totale è dispari!</h1>`
+
+        } else {
+
+            verdetto.innerHTML = `<h1 class = 'text-success'>Il totale è dispari!</h1>`
+
+        }
+
+    }
+
+}
+
+
+gioca.addEventListener('click', function(){
+
+    const hand = Number(document.getElementById('hand').value);
+
+    somma(hand, pcHand());
+
+})
+
